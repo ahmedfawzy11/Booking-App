aeb226839847185f5007dabd744921d794ce6f01
AHMED FAWZY <ahmedmido_fawzy@yahoo.com>
2022-09-18 16:49:57 +0200

    first commit

--- lib/Core/API/Error/exceptions.dart
+++ lib/Core/API/Error/exceptions.dart
@@ -1,19 +1,1 @@
-import 'package:weather_application/Feature/Data/Models/errorMessageModel.dart';
 
-class ServerException implements Exception {
-  final ErrorMessageModel errorMessage;
-
-  const ServerException(
-    this.errorMessage,
-  );
-}
-
-class LocalDatabaseException implements Exception {
-  final String message;
-
-  const LocalDatabaseException(
-    this.message,
-  );
-}
-
-class LocationDisabledException implements Exception {}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return MaterialApp(
+      title: 'Booking App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
